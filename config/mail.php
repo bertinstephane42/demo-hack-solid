9d2ce3da82b0e9d101300ed3f29043ea7604a815
bertinstephane42 <73121940+bertinstephane42@users.noreply.github.com>
2026-09-12 08:55:11 +0200

    Add files via upload

--- config/mail.php
+++ config/mail.php
@@ -3,4 +3,5 @@ return [
     'from' => env('MAIL_FROM', 'contact@cours-reseaux.fr'),
     'to' => env('MAIL_TO', 'contact@cours-reseaux.fr'),
     'from_name' => 'Cours-Reseaux',
+    'log_enabled' => (bool) env('MAIL_LOG_ENABLED', false),
 ];
