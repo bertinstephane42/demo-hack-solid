d808d4ac477aa4a09974f66f2ae9304cdb1f21b0
bertinstephane42 <73121940+bertinstephane42@users.noreply.github.com>
2026-09-17 09:53:23 +0200

    Update admin.php

--- config/admin.php
+++ config/admin.php
@@ -4,5 +4,5 @@ return [
     // Compte administrateur de la section "Envoi des mails".
     // Générer un nouveau hash avec : password_hash('VotreMotDePasse', PASSWORD_BCRYPT)
     'admin_email' => 'admin@cours-reseaux.fr',
-    'password_hash' => '$2y$10$qAznw2Q2PldFEU6ZsTpg7eNHuQ5LAbrDZQqoasEGJWB6Gh.mrNrie',
+    'password_hash' => '',
 ];
