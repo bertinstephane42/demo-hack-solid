4b3d61fab33c20c594aef70c90aba95f66d227ec
bertinstephane42 <73121940+bertinstephane42@users.noreply.github.com>
2026-09-18 21:43:06 +0200

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -1,15 +1,40 @@
 <?php
 
 // --- Détection dynamique du basePath ---
+// La position réelle du contrôleur frontal sert de référence. On n'utilise
+// SCRIPT_NAME que lorsqu'il désigne réellement le fichier (le serveur intégré
+// php -S reflète l'URI demandé dans SCRIPT_NAME, ce qui fausserait la
+// détection d'un chemin comme /admin/dashboard).
+$scriptFilename = str_replace('\\', '/', $_SERVER['SCRIPT_FILENAME'] ?? '');
+$documentRoot = str_replace('\\', '/', $_SERVER['DOCUMENT_ROOT'] ?? '');
 $scriptName = str_replace('\\', '/', $_SERVER['SCRIPT_NAME'] ?? '');
-$scriptDir = dirname($scriptName);
 $uri = $_SERVER['REQUEST_URI'] ?? '/';
 
+$publicPath = '';
+$scriptBase = basename($scriptFilename !== '' ? $scriptFilename : 'index.php');
+
+if ($scriptFilename !== '' && $documentRoot !== '') {
+    $root = rtrim($documentRoot, '/');
+    if (str_starts_with($scriptFilename, $root . '/')) {
+        $dir = dirname(substr($scriptFilename, strlen($root)));
+        if ($dir !== '/' && $dir !== '' && $dir !== '.') {
+            $publicPath = $dir;
+        }
+    }
+}
+
+if ($publicPath === '' && str_ends_with($scriptName, '/' . $scriptBase)) {
+    $dir = dirname($scriptName);
+    if ($dir !== '/' && $dir !== '.' && $dir !== '') {
+        $publicPath = $dir;
+    }
+}
+
 // Normaliser l'URI pour le routing
-if ($scriptDir !== '/' && $scriptDir !== '.') {
-    if (str_starts_with($uri, $scriptDir . '/')) {
-        $uri = substr($uri, strlen($scriptDir));
-    } elseif ($uri === $scriptDir || str_ends_with($uri, 'index.php')) {
+if ($publicPath !== '') {
+    if (str_starts_with($uri, $publicPath . '/')) {
+        $uri = substr($uri, strlen($publicPath));
+    } elseif ($uri === $publicPath || str_ends_with($uri, 'index.php')) {
         $uri = '/';
     }
 }
@@ -25,7 +50,6 @@ if ($uri === '' || $uri === 'index.php') {
 }
 
 // Stocker le basePath pour les helpers et le JS
-$publicPath = ($scriptDir !== '/' && $scriptDir !== '.') ? $scriptDir : '';
 $_SERVER['REQUEST_URI'] = $uri;
 $_SERVER['APP_BASE_PATH'] = $publicPath;
 
